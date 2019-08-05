b0d521f5642f9ce6d774a1bfe1a59970e76e8626
mob-sakai <sakai@mobcast.jp>
2019-08-05 19:01:44 +0900

    fix release script

--- deploy.sh
+++ deploy.sh
@@ -1,78 +1,97 @@
-#!/bin/sh
+#!/bin/bash -e
+
+# NOTE: Run the following command at the prompt
+#   bash <(curl -sL 'https://gist.github.com/mob-sakai/a883999a32dd8b1927639e46b3cd6801/raw/unity_release.sh')
+# NOTE: Set an environment variable `CHANGELOG_GITHUB_TOKEN` by running the following command at the prompt, or by adding it to your shell profile (e.g., ~/.bash_profile or ~/.zshrc):
+#   export CHANGELOG_GITHUB_TOKEN="«your-40-digit-github-token»"
+
+
 
 # 1. << Input release version >>
-echo ">> Start Github Release:"
+echo -e ">> Start Github Release:"
 PACKAGE_NAME=`node -pe 'require("./package.json").name'`
-echo ">> Package name: ${PACKAGE_NAME}"
+echo -e ">> Package name: ${PACKAGE_NAME}"
+PACKAGE_SRC=`node -pe 'require("./package.json").src'`
+echo -e ">> Package src: ${PACKAGE_SRC}"
 CURRENT_VERSION=`grep -o -e "\"version\".*$" package.json | sed -e "s/\"version\": \"\(.*\)\".*$/\1/"`
-UNITY_PACKAGE_SRC=`node -pe 'require("./package.json").src'`
-echo ">> Package source: ${UNITY_PACKAGE_SRC}"
-[ ! -d "$UNITY_PACKAGE_SRC" ] && echo -e "\n>> Error : $UNITY_PACKAGE_SRC is not exist." && exit 1
 
-read -p "[? (1/7) Input release version (for current: ${CURRENT_VERSION}): " RELEASE_VERSION
-[ -z "${RELEASE_VERSION}" ] && exit 1
+read -p "[? (1/8) Input release version (for current: ${CURRENT_VERSION}): " RELEASE_VERSION
+[ -z "${RELEASE_VERSION}" ] && exit
 
 read -p "[? Are the issues on this release closed all? (y/N):" yn
-case "$yn" in [yY]*) ;; *) exit 1;; esac
+case "$yn" in [yY]*) ;; *) exit ;; esac
+
+read -p "[? Is package editor only? (y/N):" yn
+case "$yn" in [yY]*) EDITOR_ONLY=true;; *) ;; esac
 
 read -p "[? Is package for UnityPackageManager? (y/N):" yn
 case "$yn" in [yY]*) UNITY_PACKAGE_MANAGER=true;; *) ;; esac
 
-echo ">> OK"
+echo -e ">> OK"
 
 
 
 # 2. << Update version in package.json >>
-echo "\n>> (2/7) Update version... package.json"
+echo -e "\n>> (2/8) Update version... package.json"
+git checkout -B release develop
 sed -i '' -e "s/\"version\": \(.*\)/\"version\": \"${RELEASE_VERSION}\",/g" package.json
-echo ">> OK"
+echo -e ">> OK"
 
 
 
-# 3. << Generate change log >>
+# 4. << Generate change log >>
 CHANGELOG_GENERATOR_ARG=`grep -o -e ".*git\"$" package.json | sed -e "s/^.*\/\([^\/]*\)\/\([^\/]*\).git.*$/--user \1 --project \2/"`
 CHANGELOG_GENERATOR_ARG="--future-release ${RELEASE_VERSION} ${CHANGELOG_GENERATOR_ARG}"
-echo "\n>> (3/7) Generate change log... ${CHANGELOG_GENERATOR_ARG}"
+echo -e "\n>> (4/8) Generate change log... ${CHANGELOG_GENERATOR_ARG}"
 github_changelog_generator ${CHANGELOG_GENERATOR_ARG}
 
 git diff -- CHANGELOG.md
 read -p "[? Is the change log correct? (y/N):" yn
-case "$yn" in [yY]*) ;; *) exit 1;; esac
-echo ">> OK"
+case "$yn" in [yY]*) ;; *) exit ;; esac
+echo -e ">> OK"
 
 
 
-# 4. << Commit release documents >>
-echo "\n>> (4/7) Commit release documents..."
-cp -f package.json CHANGELOG.md README.md "$UNITY_PACKAGE_SRC"
+# 6. << Commit release files >>
+echo -e "\n>> (6/8) Commit release files..."
+cp -f package.json CHANGELOG.md README.md $PACKAGE_SRC
 git add -u
 git commit -m "update documents for $RELEASE_VERSION"
-echo ">> OK"
+echo -e ">> OK"
 
 
 
-#  5. << Split to upm >>
+#  7. << Split for upm >>
 if [ "$UNITY_PACKAGE_MANAGER" == "true" ]; then
-  echo "\n>> (5/7) Split to upm..."
+  echo -e "\n>> Split for upm..."
   git fetch
   git show-ref --quiet refs/remotes/origin/upm && git branch -f upm origin/upm
-  ./git-subtree-split-squash.sh --prefix="$UNITY_PACKAGE_SRC" --message="$RELEASE_VERSION" --branch=upm
+  git-snapshot --prefix="$PACKAGE_SRC" --message="$RELEASE_VERSION" --branch=upm
   git push origin upm
 fi
 
 
 
-# 6. << Push to remote >>
-echo "\n>> (6/7) Push to remote..."
-git push origin
-echo ">> OK"
+# 7. << Merge and push master and develop branch >>
+echo -e "\n>> (7/8) Merge and push..."
+git checkout master
+git merge --no-ff release -m "release $RELEASE_VERSION"
+git branch -D release
+git push origin master
+git checkout develop
+git merge --ff master
+git push origin develop
+echo -e ">> OK"
 
 
 
-# 7. << Release on Github >>
-echo "\n>> (7/7) Release on Github..."
-GH_RELEASE_ARG="--name $RELEASE_VERSION --tag_name $RELEASE_VERSION"
-[ "$UNITY_PACKAGE_MANAGER" == "true" ] && GH_RELEASE_ARG="$GH_RELEASE_ARG --target_commitish upm"
-gh-release $GH_RELEASE_ARG
-[ "$?" == "0" ] && echo "\n\n>> $PACKAGE_NAME $RELEASE_VERSION has been successfully released!\n" && exit 1
-echo ">> OK"
+# 8. << Upload unitypackage and release on Github >>
+echo -e "\n>> (8/8) Releasing..."
+[ "$UNITY_PACKAGE_MANAGER" == "true" ] && git checkout upm -f
+gh-release --name $RELEASE_VERSION --tag_name $RELEASE_VERSION
+echo -e ">> OK"
+
+
+
+
+echo -e "\n\n>> $PACKAGE_NAME $RELEASE_VERSION has been successfully released!\n"
