85a756c3bd893dae8f83f070ab86fd81dab366c0
mob-sakai <sakai861104@gmail.com>
2020-08-26 16:37:53 +0900

    feat: iterative package resolution

--- Packages/com.coffee.git-dependency-resolver/Editor/GitDependencyResolver.cs
+++ Packages/com.coffee.git-dependency-resolver/Editor/GitDependencyResolver.cs
@@ -19,14 +19,10 @@ namespace Coffee.GitDependencyResolver
 
         private static PackageMeta[] GetInstalledPackages()
         {
-            /*return Directory.GetDirectories ("./Library/PackageCache")
-                .Concat (Directory.GetDirectories ("./Packages"))
-                .Select (PackageMeta.FromPackageDir)    // Convert to PackageMeta
-                .Where (x => x != null)                 // Skip null
-                .ToArray ();*/
-            return AssetDatabase.GetAllAssetPaths()
-                .Where(x => x.StartsWith("Packages/", Ordinal) && x.EndsWith("/package.json", Ordinal))
-                .Select(PackageMeta.FromPackageJson) // Convert to PackageMeta
+            return Directory.GetDirectories("./Library/PackageCache")
+                .Concat(Directory.GetDirectories("./Packages"))
+                .Select(PackageMeta.FromPackageDir) // Convert to PackageMeta
+                .Concat(new[] {PackageMeta.FromPackageJson("./Packages/manifest.json")})
                 .Where(x => x != null) // Skip null
                 .ToArray();
         }
@@ -70,56 +66,89 @@ namespace Coffee.GitDependencyResolver
 
         private static void StartResolve()
         {
-            // Uninstall unused packages (for auto-installed packages)
-            UninstallUnusedPackages();
+            var needToRefresh = false;
+            var needToCheck = true;
 
-            // Collect all installed pakages.
-            PackageMeta[] installedPackages = GetInstalledPackages();
-
-            // Collect all dependencies.
-            var dependencies = installedPackages
-                .SelectMany(x => x.dependencies) // Get all dependencies
-                .Where(x => !string.IsNullOrEmpty(x.path)); // path (url) is available
-
-            List<PackageMeta> requestedPackages = new List<PackageMeta>();
-
-            // Check all dependencies.
-            foreach (var dependency in dependencies)
+            AssetDatabase.StartAssetEditing();
+            while (needToCheck)
             {
-                // Is the depended package installed already?
-                bool isInstalled = installedPackages
-                    .Concat(requestedPackages)
-                    .Any(x => dependency.name == x.name && ((dependency.version != null && dependency.version <= x.version) || (dependency.version == null)));
+                // Uninstall unused packages (for auto-installed packages)
+                UninstallUnusedPackages();
 
-                // Install the depended package later.
-                if (!isInstalled)
+                // Collect all installed packages.
+                PackageMeta[] installedPackages = GetInstalledPackages();
+
+                // Collect all dependencies.
+                var dependencies = installedPackages
+                    .SelectMany(x => x.GetAllDependencies()) // Get all dependencies
+                    .Where(x => !string.IsNullOrEmpty(x.url)) // path (url) is available
+                    .ToArray();
+
+                // Check all dependencies.
+                var requestedPackages = new List<PackageMeta>();
+                foreach (var dependency in dependencies)
                 {
+                    // Is the depended package installed already?
+                    var isInstalled = installedPackages
+                        .Concat(requestedPackages)
+                        .Any(x => dependency.name == x.name && (dependency.version != null && dependency.version <= x.version || x.version == null));
+
+                    if (isInstalled) continue;
+
+                    // Install the depended package later.
                     requestedPackages.RemoveAll(x => dependency.name == x.name);
                     requestedPackages.Add(dependency);
                 }
-            }
 
-            // No packages is requested to install.
-            if (requestedPackages.Count == 0)
-                return;
-
-            // Install all requested packages.
-            for (int i = 0; i < requestedPackages.Count; i++)
-            {
-                PackageMeta meta = requestedPackages[i];
-                EditorUtility.DisplayProgressBar("Clone Package", string.Format("Cloning {0}:{1}", meta.name, meta.version), i / (float) requestedPackages.Count);
-                Debug.LogFormat("[Resolver] Cloning {0}: {1}", meta.name, meta.version);
-                bool success = GitUtils.ClonePackage(meta);
-                if (!success)
-                {
-                    Debug.LogFormat("[Resolver] Failed to clone {0}:{1}", meta.name, meta.version);
+                // No packages is requested to install.
+                if (requestedPackages.Count == 0)
                     break;
+
+                // Install all requested packages.
+                for (var i = 0; i < requestedPackages.Count; i++)
+                {
+                    PackageMeta package = requestedPackages[i];
+                    var clonePath = Path.GetTempFileName() + "_";
+
+                    EditorUtility.DisplayProgressBar("Clone Package", string.Format("Cloning {0}@{1}", package.name, package.version), i / (float) requestedPackages.Count);
+                    var success = GitUtils.ClonePackage(package, clonePath);
+
+                    // Check cloned
+                    if (!success)
+                    {
+                        needToCheck = false;
+                        continue;
+                    }
+
+                    // Check package path (query)
+                    var pkgPath = package.GetPackagePath(clonePath);
+                    PackageMeta newPackage = PackageMeta.FromPackageDir(pkgPath);
+                    if (!Directory.Exists(pkgPath) || newPackage == null || newPackage.name != package.name)
+                    {
+                        needToCheck = false;
+                        continue;
+                    }
+
+                    // Install as an embed package
+                    var installPath = "Packages/." + newPackage.GetDirectoryName();
+                    DirUtils.Delete(installPath);
+                    DirUtils.Create(installPath);
+                    DirUtils.Move(pkgPath, installPath, p => p != ".git");
+                    AssetDatabase.ImportAsset(installPath, ImportAssetOptions.ForceUpdate | ImportAssetOptions.ForceSynchronousImport | ImportAssetOptions.ImportRecursive);
+
+                    needToRefresh = true;
                 }
+
+                EditorUtility.ClearProgressBar();
             }
 
-            // Recompile the packages
-            EditorUtility.ClearProgressBar();
-            EditorApplication.delayCall += AssetDatabase.Refresh;
+            AssetDatabase.StopAssetEditing();
+
+            if (needToRefresh)
+            {
+                // Recompile the packages
+                AssetDatabase.Refresh();
+            }
         }
     }
 }
